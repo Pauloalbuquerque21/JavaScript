5143708b6f0d650c0ef8bc37f38dd9a88515bb8c
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-08-16 09:17:53 -0300

    continuo na quinta

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -3,4 +3,4 @@ console.log('A velocidade do se carro é '+vel+'km/h')
     if(vel > 60){
         console.log('Você ultrapassou a velocidade de segurança, foi multado')
     }
-console.log('Use cinto de segurança')
+console.log('Use cinto de segurança') 
