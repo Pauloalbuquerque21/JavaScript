59634b670490a9de1e1bde338c4e91c095a9f681
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-11-26 21:25:29 -0300

    fazendo o desafio funções

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -2,4 +2,8 @@ function tocasompom () {
     document.querySelector('#som_tecla_pom').play();  
 }
 
+function tocaSomClap (){
+    document.querySelector().play();
+}
+
 document.querySelector('.tecla_pom');
