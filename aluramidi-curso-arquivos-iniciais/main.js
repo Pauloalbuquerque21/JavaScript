d2fa432431f8cb4d91a2fcd3b65505be11012f44
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-11-26 21:49:29 -0300

    em processo

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -6,4 +6,5 @@ function tocaSomClap (){
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_pom');
+document.querySelector('.tecla_pom').onclick=tocasompom;
+document.querySelector('.tecla_clap').onclick=tocaSomClap;
