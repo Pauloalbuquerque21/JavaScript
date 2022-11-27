6d9394f9cca28ba0d60b25379d3fa7d88efe2a64
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-11-27 17:59:49 -0300

    usei o selector all

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,40 +1,5 @@
 function tocasompom () {
     document.querySelector('#som_tecla_pom').play();  
 }
-
-function tocaSomClap (){
-    document.querySelector('#som_tecla_clap').play();
-}
-function tocasomtim(){
-    document.querySelector('#som_tecla_tim').play();
-
-}
-function tocasompuff(){
-    document.querySelector('#som_tecla_puff').play();
-
-}
-function tocasomsplash(){
-    document.querySelector('#som_tecla_splash').play();
-}
-function tocasomtoim (){
-    document.querySelector('#som_tecla_toim').play()
-}
-function tocasompsh(){
-    document.querySelector('#som_tecla_psh').play()
-}
-function tocasomtic(){
-    document.querySelector('#som_tecla_tic').play()
-}
-function tocasomtom(){
-    document.querySelector('#som_tecla_tom').play()
-}
-
-document.querySelector('.tecla_pom').onclick=tocasompom;
-document.querySelector('.tecla_clap').onclick=tocaSomClap;
-document.querySelector('.tecla_tim').onclick=tocasomtim;
-document.querySelector('.tecla_puff').onclick=tocasompuff;
-document.querySelector('.tecla_splash').onclick=tocasomsplash;
-document.querySelector('.tecla_toim').onclick=tocasomtoim;
-document.querySelector('.tecla_psh').onclick=tocasompsh;
-document.querySelector('.tecla_tic').onclick=tocasomtic;
-document.querySelector('.tecla_tom').onclick=tocasomtom;
+ 
+document.querySelectorAll('.tecla')
