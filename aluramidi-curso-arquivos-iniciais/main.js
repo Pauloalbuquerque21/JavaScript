e5c674ec13e905d30922265b55dbb7b4ea6eb701
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2022-12-02 09:23:45 -0300

    iniciando o percorrendo uma lista

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,6 +1,13 @@
+// POM ----------------------------------------------------
 function tocasompom () {
     document.querySelector('#som_tecla_pom').play();  
 }
- 
 const listadeteclas = document.querySelectorAll('.tecla'); 
 listadeteclas[0].onclick = tocasompom;
+
+let contador = 0;
+
+//enquanto 
+while (contador < 9) {
+    
+}
