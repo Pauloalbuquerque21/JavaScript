d2fad6d0715df12ce7b7a1cf343bd87d589d2b13
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-09 11:20:12 -0300

    em processo

---
+++ aula 11/ex011.js
@@ -0,0 +1,21 @@
+var idade = 18
+if (idade<16){
+    console.log('Não vota')
+} else{
+    if (idade<18 || idade>65){
+        console.log('voto opcional')}
+    else{
+        console.log('Obrigado a votar')
+    }
+}
+
+var idade = 
+
+console.log(´você tem ${idade} anos´)
+if (idade<16){
+    console.log('Não vota')
+}else if(idade<18 || idade > 65){
+    console.log('Voto opcional')
+}else{
+    console.log('Voto obrigatorio')
+}
