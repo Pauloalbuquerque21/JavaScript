63138f9a93bc3e53b0d6b7d402b814d48ed29365
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-10 13:19:28 -0300

    em processo

--- aula 11/ex12.js
+++ aula 11/ex12.js
@@ -1,21 +1,21 @@
-//var hora = 12
-//console.log(`Agora são exatamente ${hora} horas`)
-//if (hora < 12){
-//    console.log('Bom dia!')
-//}else if (hora <= 18){
-//    console.log('Boa Tarde!')
-//}else{
-//    console.log('Boa Noite!')
-//}
-
 var hora = 12
-var dia = 'pm'
+console.log(`Agora são exatamente ${hora} horas`)
+if (hora < 12){
+    console.log('Bom dia!')
+}else if (hora <= 18){
+    console.log('Boa Tarde!')
+}else{
+    console.log('Boa Noite!')
+}
+
+var hora = 5
+var dia = 'am'
 console.log(`Agora são exatamente ${hora} ${dia} houres`)
 if (hora < 12 && dia == 'am'){
     console.log('Good morning!')
 }else if (hora < 6 && dia =='pm'){
     console.log('Good afternoon!')
-}else (6<=hora<=12 && dia =='pm')
+}else
 {
     console.log('Good evening')
 }
