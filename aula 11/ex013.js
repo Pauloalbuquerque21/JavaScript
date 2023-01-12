ad501db3ad482ecbe85661647c6639e2b422df6d
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-12 10:22:02 -0300

    fiz exercicios de condicionais

--- aula 11/ex013.js
+++ aula 11/ex013.js
@@ -38,9 +38,9 @@ switch(diaSem){
         break
 
 
-}*/
+}
 
-var idade = 12
+var idade = 19
 
 if(idade <18){
     console.log("Você é menor de idade, ou seja, não pode entrar")
@@ -49,4 +49,42 @@ if(idade <18){
     console.log('Você é maior de idade, ou seja, pode entrar')
 
 }
+var nacional = 'frances'
+var motivo = 'casamento'
+
+if (nacional == 'brasil'){
+    console.log('Seja bem vido, ao seu país')}
+else if (motivo == 'estudos' || motivo == 'ferias') {
+        console.log('Você é estrangeiro, porem pode entrar, pois você tem um bom motivo para entrar ao Brasil')}
+    else{
+            console.log('[Não pode entrar]')
+        }*/
+var veiculo = 'trator'
+        /*
+        carro = 10 reias
+        moto = 5 reias
+        bicicleta = 2 reais 
+        trator = 50 reias
+        avião = 1000 reias
+        */
+       switch(veiculo){
+        case 'carro':
+            console.log('10 reias')
+            break
+        case 'Moto':
+            console.log('5 reais')
+            break
+        case 'bicicleta':
+            console.log('2 reais')
+            break
+        case 'trator':
+            console.log('50 reais')
+            break
+        case 'avião':
+            console.log('1000 reais')
+            break
+        default:
+            console.log('[ERRO] Veículo não encontrado')
+       } 
+    
 
