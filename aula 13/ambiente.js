7fc85745fe9523264785a3b04acd9ad6f5b55cc5
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-18 09:18:01 -0300

    indo para repetições parte 2

--- aula 13/ambiente.js
+++ aula 13/ambiente.js
@@ -7,13 +7,17 @@ while (c<6){
 var a = 1
 var e = 10
 console.log('Tem pão[yes or not]')
-var c = 'yes'
+var c = 'not'
 if (c=='yes'){
     console.log('Quantos Paes')
     var b = 6 
-}else(b<=e){
+}else if(b<=e)
+{
     do{
         console.log(`Temos Pão ${a}`)
         a=a+1
     }while(a<=b)
+}
+else {
+    console.log('Desculpa, não tem pão')
 }
