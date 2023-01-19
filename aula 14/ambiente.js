5502e91e3c93c35aecfb325aaf4b6edf2b94cb49
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-19 10:39:57 -0300

    pratei um pouco de repetidor e condicionamento

--- aula 14/ambiente.js
+++ aula 14/ambiente.js
@@ -6,10 +6,33 @@ do{
 }while(c<=5)
 */
 //-------------
-console.log('Vai começar...')
+
+/*console.log('Vai começar...')
 
 for (var c=1;c <= 5;c++){
     console.log(c)
 }
-console.log('Fim')
+console.log('Fim')*/
+
+console.log('Padaria')
+console.log('Quantos paes:')
+var d = 1
+console.log('Quantos paes o senhor deseja?')
+var a = 4
+if(d=>a){
+    for (c=1; c<=a;c++){
+        console.log(`Paes que são conlosocados na sacola ${c}`)
+        
+    }
+    var e = d-a
+    console.log(`Restou ${e} paes`)
+}else if(d<a && d!=0){
+    console.log(`Desculpa, o senhor quer ${a}paes, porem só temos ${d}`)
+}else{
+    (`Desculpa senhor, não temos paes`)
+}
+
+
+
+
 
