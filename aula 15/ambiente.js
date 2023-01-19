8cb0d34573aa8385b68176372fbb77b04c751284
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-19 13:28:28 -0300

    em processo com o array

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -1,10 +1,16 @@
- let num = [5,6,7,8,9]
+ /*let num = [5,6,7,8,9]
 
-/* console.log(num[1]) */
+/* console.log(num[1]) 
 num.push(7)
 console.log(num[5])
 num.lenth
 console.log(num.length)
 num.sort()
-console.log(num.sort())
+console.log(num.sort())*/
+let num = [5,8,2,9,3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} `)
+console.log(`O elemento do índice 2 é igual a ${num[2]}`)
 
