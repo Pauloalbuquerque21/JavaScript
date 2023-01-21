f2d7316e59a97343f739f8dfa28107c4686e3cf3
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-01-21 12:28:13 -0300

    terminei o desafio

--- praticando/padaria.js
+++ praticando/padaria.js
@@ -1,20 +1,20 @@
 let paes = ["pão francês","Pão doce","pão de alho","pão carteira"]
 let valores =["1,00","1,50","2,00","1,50"]
-console.log(`Hoje temos ${pães.length} tipos de pães `)
+var c = 0
+console.log(`Hoje temos ${paes.length} tipos de pães `)
 /*for(let num in pães){
     console.log(`${pães[num]}:`)
     for(let num1 in valores){
         Console.log(`${valores[num1]} Reais`)
     }
 }*/
-/*
-console.log(`${pães.length}`)
-console.log(`${pães}`)
-while(c<=pães.length){
-    console.log(`${pães[c]}:`)
+while(c<=paes.length){
+    console.log(`${paes[c]}:`)
+    console.log(`${valores[c]}`)
     c++
 
-}*/
+}
+/*
 for(let num in paes){
     console.log(`${paes[num]}:\n ${valores[num]}`)
-}
+}*/
