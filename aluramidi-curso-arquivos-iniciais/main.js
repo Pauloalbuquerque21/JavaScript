531440fbcc6c5c5b9528f09b0c7dac88db1ed534
Paulo Vitor Albuquerque Silva <paulo.vitor21@hotmail.com>
2023-02-02 08:49:34 -0300

    colocando audio nos botons

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -3,3 +3,21 @@ function tocaPom() {
     document.querySelector('#som_tecla_pom').play();  
 }
 document.querySelector('.tecla_pom').onclick = tocaPom
+
+
+function tocaclap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = tocaclap
+
+
+function tocatim(){
+    document.querySelector('#som_tecla_tim').play()
+}
+document.querySelector('.tecla_tim').onclick = tocatim
+
+
+function tocapuff(){
+    document.querySelector('#som_tecla_puff').play()
+}
+document.querySelector('.tecla_puff').onclick = tocapuff
